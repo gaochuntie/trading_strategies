09475e6226d075539923e821b85276c1278c358e
gaochuntie <2041469901@qq.com>
2024-01-28 22:02:25 +0800

    Add gold market sleep support

--- src/main/java/org/example/DayMarketState.java
+++ src/main/java/org/example/DayMarketState.java
@@ -1,13 +1,15 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class DayMarketState {
     public String date = String.valueOf(UUID.randomUUID());
-    public double gold_real=-1;
-    public double gold_guess=-1;
-    public double bchain_real=-1;
-    public double bchain_guess=-1;
+    public double gold_real = -1;
+    public double gold_guess = -1;
+    public double bchain_real = -1;
+    public double bchain_guess = -1;
     public int bchain_index = -1;
     DayMarketState next = null;
     DayMarketState previous = null;
@@ -40,45 +42,113 @@ public class DayMarketState {
     }
 
     public static boolean isGuessGoldPriceGoingUp(DayMarketState dayMarketState) {
-        if (dayMarketState != null && dayMarketState.previous != null) {
-            if (dayMarketState.gold_guess> dayMarketState.previous.gold_real) {
+        DayMarketState yesterday=dayMarketState.previous;
+        while (yesterday!=null){
+            if(yesterday.gold_real==-1){
+                yesterday=yesterday.previous;
+            }else {
+                break;
+            }
+        }
+        if (dayMarketState != null && yesterday != null) {
+            if (dayMarketState.gold_guess > yesterday.gold_real) {
                 return true;
             }
         }
         return false;
     }
+
     public static boolean isGuessBchainPriceGoingUp(DayMarketState dayMarketState) {
         if (dayMarketState != null && dayMarketState.previous != null) {
-            if (dayMarketState.bchain_guess> dayMarketState.previous.bchain_real) {
+            if (dayMarketState.bchain_guess > dayMarketState.previous.bchain_real) {
                 return true;
             }
         }
         return false;
     }
+
     public static boolean isGuessGoldPriceGoingDown(DayMarketState dayMarketState) {
-        if (dayMarketState != null && dayMarketState.previous != null) {
-            if (dayMarketState.gold_guess< dayMarketState.previous.gold_real) {
+        DayMarketState yesterday=dayMarketState.previous;
+        while (yesterday!=null){
+            if(yesterday.gold_real==-1){
+                yesterday=yesterday.previous;
+            }else {
+                break;
+            }
+        }
+        if (dayMarketState != null && yesterday != null) {
+            if (dayMarketState.gold_guess < yesterday.gold_real) {
                 return true;
             }
         }
         return false;
     }
+
     public static boolean isGuessBchainPriceGoingDown(DayMarketState dayMarketState) {
         if (dayMarketState != null && dayMarketState.previous != null) {
-            if (dayMarketState.bchain_guess< dayMarketState.previous.bchain_real) {
+            if (dayMarketState.bchain_guess < dayMarketState.previous.bchain_real) {
                 return true;
             }
         }
         return false;
     }
-    public static boolean isGuessGoldPriceGrowthOverThreshold(DayMarketState dayMarketState, double threshold) {
-        if (dayMarketState != null && dayMarketState.previous != null) {
-            if (dayMarketState.gold_guess > dayMarketState.previous.gold_real * (1 + threshold)) {
-                return true;
+
+    public static boolean isGuessGoldPriceContinueGrowth_TotallyOverThreshold(DayMarketState dayMarketState, double threshold, int days) {
+        List<Double> price_history = new ArrayList<>();
+        DayMarketState pointer = dayMarketState.previous;
+        price_history.add(dayMarketState.gold_guess);
+        for (int i = 2; i <= days; i++) {
+            if (pointer != null) {
+                // market sleep
+                if (pointer.gold_real == -1) {
+                    i--;
+                    pointer = pointer.previous;
+                    continue;
+                }
+                //check is going up
+                if (pointer.gold_real >= price_history.get(i - 2)) {
+                    return false;
+                }
+                price_history.add(pointer.gold_real);
+                pointer = pointer.previous;
+            } else {
+                return false;
             }
+        }
+        if (price_history.get(0) > price_history.get(days - 1) * (1 + threshold)) {
+            return true;
         }
         return false;
     }
+
+    public static boolean isGuessBchainPriceContinueGrowth_TotallyOverThreshold(DayMarketState dayMarketState, double threshold, int days) {
+        List<Double> price_history = new ArrayList<>();
+        DayMarketState pointer = dayMarketState.previous;
+        price_history.add(dayMarketState.bchain_guess);
+        for (int i = 2; i <= days; i++) {
+            if (pointer != null) {
+                // market sleep
+                if (pointer.bchain_real == -1) {
+                    i--;
+                    pointer = pointer.previous;
+                    continue;
+                }
+                //check is going up
+                if (pointer.bchain_real >= price_history.get(i - 2)) {
+                    return false;
+                }
+                price_history.add(pointer.bchain_real);
+                pointer = pointer.previous;
+            } else {
+                return false;
+            }
+        }
+        if (price_history.get(0) > price_history.get(days - 1) * (1 + threshold)) {
+            return true;
+        }
+        return false;
+    }
+
     public static boolean isGuessBchainPriceGrowthOverThreshold(DayMarketState dayMarketState, double threshold) {
         if (dayMarketState != null && dayMarketState.previous != null) {
             if (dayMarketState.bchain_guess > dayMarketState.previous.bchain_real * (1 + threshold)) {
@@ -87,17 +157,92 @@ public class DayMarketState {
         }
         return false;
     }
-    public static boolean isGuessGoldFallOverThreshold(DayMarketState dayMarketState, double threshold) {
-        if (dayMarketState != null && dayMarketState.previous != null) {
-            if (dayMarketState.gold_guess < dayMarketState.previous.gold_real * (1 - threshold)) {
+
+    public static boolean isGuessGoldPriceGrowthOverThreshold(DayMarketState dayMarketState, double threshold) {
+        DayMarketState yesterday=dayMarketState.previous;
+        while (yesterday!=null){
+            if(yesterday.gold_real==-1){
+                yesterday=yesterday.previous;
+            }else {
+                break;
+            }
+        }
+        if (dayMarketState != null && yesterday != null) {
+            if (dayMarketState.gold_guess > yesterday.gold_real * (1 + threshold)) {
                 return true;
             }
         }
         return false;
     }
+
+    public static boolean isGuessGoldFallOverThreshold(DayMarketState dayMarketState, double threshold) {
+        DayMarketState yesterday=dayMarketState.previous;
+        while (yesterday!=null){
+            if(yesterday.gold_real==-1){
+                yesterday=yesterday.previous;
+            }else {
+                break;
+            }
+        }
+        if (dayMarketState != null && yesterday != null) {
+            if (dayMarketState.gold_guess < yesterday.gold_real * (1 - threshold)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static boolean isGuessBchainFallOverThreshold(DayMarketState dayMarketState, double threshold) {
         if (dayMarketState != null && dayMarketState.previous != null) {
             if (dayMarketState.bchain_guess < dayMarketState.previous.bchain_real * (1 - threshold)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isGuessGoldFallFromHistoryTopOverThreshold(DayMarketState today, DayMarketState history_top, double threshold) {
+        if (today != null && history_top != null) {
+            if (history_top.gold_real <= 0) {
+                return false;
+            }
+            if (today.gold_guess < history_top.gold_real * (1 - threshold)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isGuessBchainFallFromHistoryTopOverThreshold(DayMarketState today, DayMarketState history_top, double threshold) {
+        if (today != null && history_top != null) {
+            if (today.bchain_guess < history_top.bchain_real * (1 - threshold)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * can't be called at first day !!!!!!!!!!!!!!
+     *
+     * @param today
+     * @param cash
+     * @return
+     */
+    public static boolean isGuessGoldBuyAvaible(DayMarketState today, double cash) {
+        if (cash <= 0 || today == null) {
+            return false;
+        }
+        DayMarketState yesterday = today.previous;
+        while (yesterday != null) {
+            if (yesterday.gold_real == -1) {
+                yesterday = yesterday.previous;
+            } else {
+                break;
+            }
+        }
+        if (yesterday != null) {
+            if ((gold_handling_fee) < ((1 - gold_handling_fee) * (today.gold_guess / yesterday.gold_real - 1))) {
                 return true;
             }
         }
